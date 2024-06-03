769b6ef367fa70b6f509f544244531309bf09150
Juan Cruz Viotti <jv@jviotti.com>
2024-06-03 14:54:22 -0400

    Upgrade JSON Schema CLI to v0.1.1
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- Casks/jsonschema.rb
+++ Casks/jsonschema.rb
@@ -1,6 +1,6 @@
 cask "jsonschema" do
-  version "0.1.0"
-  sha256 "f87daa4440958d1bd2823f6713d14f345892533756adc2957c92de0d833ebdc3"
+  version "0.1.1"
+  sha256 "3feb21633894b5978951337f311f47eae8bc42d230ff58be28a2a26c162ee32d"
   url "https://github.com/Intelligence-AI/jsonschema/releases/download/v#{version}/jsonschema-#{version}-darwin-arm64.zip"
   name "JSON Schema CLI"
   desc "The CLI for working with JSON Schema"
