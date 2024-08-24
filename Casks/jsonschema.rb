794222ff721b704fbc651fc4d560642f31609ec3
Juan Cruz Viotti <jv@jviotti.com>
2024-08-23 22:20:24 -0400

    Update JSON Schema CLI source links
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- Casks/jsonschema.rb
+++ Casks/jsonschema.rb
@@ -5,10 +5,10 @@ cask "jsonschema" do
   sha256 arm:   "a745b104342d7ee414d33d2f03dc8f16f3ff00984b1e72caa1e29757648feb6c",
          intel: "e52145f997848387b44a376d69bdd6347270b0fb523ec7473f039096a74965ec"
 
-  url "https://github.com/Intelligence-AI/jsonschema/releases/download/v#{version}/jsonschema-#{version}-darwin-#{arch}.zip"
+  url "https://github.com/sourcemeta/jsonschema/releases/download/v#{version}/jsonschema-#{version}-darwin-#{arch}.zip"
   name "JSON Schema CLI"
   desc "The CLI for working with JSON Schema"
-  homepage "https://github.com/Intelligence-AI/jsonschema"
+  homepage "https://github.com/sourcemeta/jsonschema"
   binary "jsonschema-#{version}-darwin-#{arch}/bin/jsonschema"
   postflight do
     system_command "xattr", args: ["-c", "#{staged_path}/jsonschema-#{version}-darwin-#{arch}/bin/jsonschema"]
