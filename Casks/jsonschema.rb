550c9b857fc3aa97c8f2905f74dfa8ba8372e40b
Juan Cruz Viotti <jv@jviotti.com>
2025-04-10 16:00:44 -0400

    JSON Schema CLI v8.1.0
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- Casks/jsonschema.rb
+++ Casks/jsonschema.rb
@@ -1,9 +1,9 @@
 cask "jsonschema" do
-  version "8.0.0"
+  version "8.1.0"
 
   arch arm: "arm64", intel: "x86_64"
-  sha256 arm:   "d630210b1a170cebe07adf8b47f22341532d4986c932687469b61e6279ff3eaa",
-         intel: "e026d79c25de33aa7c455e0c8155c96a3de42b7ffc7344c2fd94972aeeefbf6f"
+  sha256 arm:   "991a9ebc2c3c6f827a66bc110924ef634f77c075f1126541810f9d2c4f55f20b",
+         intel: "48dd69e29d14843bad574cf5abfd34d6aef3b2ffe4f1c982308791d977c4f224"
 
   url "https://github.com/sourcemeta/jsonschema/releases/download/v#{version}/jsonschema-#{version}-darwin-#{arch}.zip"
   name "JSON Schema CLI"
