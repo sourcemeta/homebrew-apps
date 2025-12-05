9435886a59c68471e8e8d96398d95e03e37b7465
Juan Cruz Viotti <jv@jviotti.com>
2025-12-05 09:34:06 -0400

    JSON Schema v12.10.1
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- Casks/jsonschema.rb
+++ Casks/jsonschema.rb
@@ -1,10 +1,10 @@
 cask "jsonschema" do
-  version "12.10.0"
+  version "12.10.1"
 
   arch arm: "arm64", intel: "x86_64"
 
-  sha256 arm:   "b6a43eadc003b175b0fb80a711ae5bfac49daf0030ecadddfd5e4209343ef43f",
-         intel: "72e071b8fcc0c2007e39b4cd130ac81d2ed1a484a190de9382986a158dce2caf"
+  sha256 arm:   "1a5abb171b78db19173ef1c70b443a5f289fd1bff5fc536909e585bf3a3b3c5d",
+         intel: "0515e7f9062534ddc1a2168415e8875f27dac8d519de9bb3b3d944e68f7e8bee"
 
   url "https://github.com/sourcemeta/jsonschema/releases/download/v#{version}/jsonschema-#{version}-darwin-#{arch}.zip"
   name "JSON Schema CLI"
